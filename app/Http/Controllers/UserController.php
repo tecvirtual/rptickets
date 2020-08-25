262f12ae3a8e92fd80b1787d86e8baa629a938b7
Frank Garcia <garcia1901l@gmail.com>
2020-08-24 21:35:25 -0500

    User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
             $users = User::all();
             return response()->json($users, 200);
         }else{
+
             return response()->json(['error' => 'Unauthorized'], 401, []);
         }
     }
